104e748222daf41f0b71bec739da99f37a8c7943
Todi Adiyatmo <todiadiyatmo@tonjoo.com>
2014-06-11 16:19:06 +0700

    fix license2

--- src/Facade.php
+++ src/Facade.php
@@ -5,8 +5,6 @@
 * @link    http://www.todiadiyatmo.com
 * @license MIT License Copyright (c) 2014 Todiadiyatmo
 *
-* This router is based on the Laravel routing system. Some code is reused.
-*
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
 * in the Software without restriction, including without limitation the rights
